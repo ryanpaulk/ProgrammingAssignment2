25574db78999049a51bcd0799b45c3de9407c7d6
ryanpaulk <ryanpaulk@gmail.com>
2020-12-03 15:57:31 -0600

    It's a start

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,34 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## Defines the get/set functions
+## Defines the matrix, x, and the inverse, s
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) { ##defines the set function
+    x <<- y
+    s <<- NULL ##assigning values in parent scopes
+  }
+  get <- function() x ##defines the get function
+  setSolve <- function(solve) x <<- solve ##define in parent scope and get/set/Solve functions
+  getSolve <- function() s
+  list(set = set, get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
 
 
 ## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
+## Solve function without additional arguments will return inverse matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x[[getSolve()]]
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x[[get()]]
+  s <- solve(data, ...)
+  x[[setSolve(s)]]
+  s
 }
